e44fccfd1a7d149edbf13a549ff7a6fab682ac9b
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2025-05-27 16:20:42 +0700

    string perkenalan

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -30,5 +30,9 @@ public:
     ~pelajar() {
         cout << "pelajar dihapus\n" << endl;
     }
+    string perkenalan() {
+        return "Hallo, nama saya " + nama + "dari sekolah " + sekolah + "\n\n";
+    }
+    };
+
     
-}
