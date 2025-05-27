58bf26b28b0d697d36ba4d4c0085dc49bf70c40d
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2025-05-27 16:16:32 +0700

    string sekolah

--- inheritance/inheritance.cpp
+++ inheritance/inheritance.cpp
@@ -13,4 +13,22 @@ public:
         ~orang() {
             cout << "orang dihapus\n" << endl;
         }
+
+        int jumlah(int a, int b) {
+            return a + b;
+        }
+
+    };
+
+class pelajar : public orang {
+public:
+    string sekolah;
+
+    pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah){
+        cout << "pelajar dibuat\n" << endl;
+    }
+    ~pelajar() {
+        cout << "pelajar dihapus\n" << endl;
+    }
+    
 }
