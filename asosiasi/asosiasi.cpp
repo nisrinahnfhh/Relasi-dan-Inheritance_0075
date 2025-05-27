c741e62c6dd7ba65f93044da91eca750400993ee
Nisrina Hanifah Ramadhani <nisrinahnfhh@gmail.com>
2025-05-27 15:52:14 +0700

    void pasien

---
+++ asosiasi/asosiasi.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class dokter; 
+class pasien { // asosiasi
+public:
+    string nama;
+    vector<dokter*> daftar_dokter;
+    pasien(string pNama) : nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+
+    ~pasien() {
+        cout << "Pasien \"" << nama 
+        << "\" tidak ada\n";
+    }
+
+    void tambahDokter(dokter* pDokter);
+    void cetakDokter();
+};
+class dokter {
+public:
+    string nama;
+    vector<pasien*> daftar_pasien;
+
+    dokter(string pNama) : nama(pNama) {
+        cout << "Dokter \"" << nama << "\" ada\n";
+    }
+     ~dokter() {
+        cout << "Dokter \"" << nama 
+        << "\" tidak ada\n";
+    }
+
+              void tambahPasien(pasien*);
+              void cetakPasien();
+};
+
+void pasien::tambahDokter(dokter* pDokter){
+    daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+    cout << "Daftar dokter yang menangani pasien \""
+    << this->nama << "\":\n";
+    //auto digunakan dalam perulangan
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
